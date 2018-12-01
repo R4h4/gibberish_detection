2ffb7fee6e1f6d0d8c788a69f5af74e92dd1a1cc
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2018-12-01 19:27:36 +0700

    test3

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -10,7 +10,7 @@ class TestHandlerCase(unittest.TestCase):
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello World', result['body'])
+        self.assertIn('Hello Test', result['body'])
 
 
 if __name__ == '__main__':
