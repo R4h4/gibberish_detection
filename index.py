0b5d20e3e3cc7b0b73e0c73203190649fbb14115
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2018-12-01 18:31:49 +0700

    test commit

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello Test',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
