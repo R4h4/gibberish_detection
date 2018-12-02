26970cdf964e2a781241307b5c42d27cdc11d7ce
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2018-12-02 18:04:35 +0700

    added File + Unit test

--- index.py
+++ index.py
@@ -1,13 +1,51 @@
 import json
 import datetime
 import pickle
+import boto3
+import math
 
 
 def handler(event, context):
-    req_name = event["queryStringParameters"]['string']
+
+    req_name = event['queryStringParameters']['string']
+
+    s3 = boto3.resource('s3')
+    with open('/tmp/gib_model.pki', 'wb') as data:
+        s3.Bucket('aws-codestar-ap-southeast-1-348342725339-gib-detect-pipe').download_fileobj('gib_model.pki', data)
+
+    model_data = pickle.load(open('/tmp/gib_model.pki', 'rb'))
+
+    accepted_chars = 'abcdefghijklmnopqrstuvwxyzöäüèéáıçğ-ßó '
+    pos = dict([(char, idx) for idx, char in enumerate(accepted_chars)])
+
+    def normalize(line):
+        return [c.lower() for c in line if c.lower() in accepted_chars]
+
+    def ngram(n, l):
+        # Return all n grams from l after normalizing
+        filtered = normalize(l)
+        for start in range(0, len(filtered) - n + 1):
+            yield ''.join(filtered[start:start + n])
+
+    def avg_transition_prob(l, log_prob_mat):
+        # Return the average transition prob from l through log_prob_mat.
+        log_prob = 0.0
+        transition_ct = 0
+        for a, b in ngram(2, l):
+            log_prob += log_prob_mat[pos[a]][pos[b]]
+            transition_ct += 1
+        # The exponentiation translates from log probs to probs.
+        return math.exp(log_prob / (transition_ct or 1))
+
+    def gibberish(name: str):
+        model_mat = model_data['mat']
+        threshold = model_data['thresh']
+        return avg_transition_prob(name, model_mat) < threshold
+
+    res = gibberish(req_name)
 
     data = {
-        'output': 'Hello Test',
+        'gibberish': res,
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -6,11 +6,11 @@ class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
-        result = index.handler({'queryStringParameters': {'string': '123'}}, None)
+        result = index.handler({'queryStringParameters': {'string': 'Karsten'}}, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello Test', result['body'])
+        self.assertIn(True, result['body'])
 
 
 if __name__ == '__main__':
