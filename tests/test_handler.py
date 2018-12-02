470b69a7f4c69627fe8baac6ea0b76c760d605b0
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2018-12-02 16:40:51 +0700

    fixed unittest

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -6,7 +6,7 @@ class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
-        result = index.handler(None, None)
+        result = index.handler({'string': '123'}, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
